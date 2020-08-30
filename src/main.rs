84ede1815ee4591bbbecb73056ac18566d475149
Ahmed Sakr <ahmed@sakr.ca>
2020-08-30 17:16:53 -0400

    Use structopt crate for parsing commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pretty-ls")]
+struct Opt {
+
+    /// Turns on debugging
+    #[structopt(short, long)]
+    debug: bool
+
+}
+
+// Entry point for the pls program
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
 }
