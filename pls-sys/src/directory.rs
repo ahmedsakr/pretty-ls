ae287e1f6281714c1fabcef75fb140601f6dc0ea
Ahmed Sakr <ahmed@sakr.ca>
2020-09-03 15:50:51 -0400

    Improve directory.rs code

--- pls-sys/src/directory.rs
+++ pls-sys/src/directory.rs
@@ -1,31 +1,38 @@
-use std::io;
+use std::io::{Error as IOError, ErrorKind};
 use std::process::Command;
 use std::str;
 
 // Lists the content of the current working directory
-pub fn list_dir(dir: Option<&String>) -> Result<Vec<String>, io::Error> {
+pub fn list_dir(dir: Option<&String>) -> Result<Vec<String>, IOError> {
+    let mut command = Command::new("ls");
+
     let out = match dir {
-        Some(directory) => Command::new("ls").arg(directory).output()?,
+        Some(directory) => command.arg(directory).output()?,
 
         // Use the current working directory by default
-        None => Command::new("ls")
-            .arg(get_current_dir()?.to_string())
-            .output()?,
+        None => command.arg(get_current_dir()?.to_string()).output()?,
     };
 
-    let files = str::from_utf8(&out.stdout)
+    println!("{}", out.status);
+
+    if let Some(0) = out.status.code() {
+
+        let files = str::from_utf8(&out.stdout)
         .expect("Failed to decode")
         .lines();
 
-    // Must convert vector Lines to a vector of Strings
-    let mut files_str: Vec<String> = Vec::new();
-    files.for_each(|file| files_str.push(file.to_string()));
+        // Must convert vector Lines to a vector of Strings
+        let mut files_str: Vec<String> = Vec::new();
+        files.for_each(|file| files_str.push(file.to_string()));
 
-    Ok(files_str)
+        Ok(files_str)
+    } else {
+        Err(IOError::new(ErrorKind::InvalidData, "ls failed"))
+    }
 }
 
 // Parses the absolute path of the current directory
-fn get_current_dir() -> Result<String, io::Error> {
+fn get_current_dir() -> Result<String, IOError> {
     let out = Command::new("pwd").output()?;
     let dir = str::from_utf8(&out.stdout).expect("Failed to decode");
 
