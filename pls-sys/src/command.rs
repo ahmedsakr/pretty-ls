ab90a6fa293fd9635fdfc802715df4bd5dc1356b
Ahmed Sakr <ahmed@sakr.ca>
2020-09-09 13:15:42 -0400

    Modify SimpleCommand run() to return vector of strings

--- pls-sys/src/command.rs
+++ pls-sys/src/command.rs
@@ -2,19 +2,19 @@ use std::io::{self, Error, ErrorKind};
 use std::process;
 
 pub trait SimpleCommand {
-    // Executes the command, returning the standad output for the
-    // command.
-    fn run(&self) -> io::Result<String>;
     // Adds an argument to the chain of arguments for this command.
     // If the argument is None, nothing should be done.
     fn add_argument(&mut self, arg: Option<String>);
+    // Executes the command, returning the standad output for the
+    // command.
+    fn run(&self) -> io::Result<Vec<String>>;
 }
 
 pub struct SystemCommand<'a> {
     // The executable name
-    pub name: &'a str,
+    name: &'a str,
     // Everything else specified after executable name
-    pub arguments: Vec<String>,
+    arguments: Vec<String>,
 }
 
 impl<'a> SystemCommand<'a> {
@@ -26,12 +26,23 @@ impl<'a> SystemCommand<'a> {
             arguments: Vec::new(),
         }
     }
+    // Transforms raw stdout byte vector into a vector of Strings.
+    fn parse_stdout(&self, raw_bytes: Vec<u8>) -> Vec<String> {
+        let mut stdout: Vec<String> = Vec::new();
+        let raw_output = String::from_utf8(raw_bytes).expect("Failed to decode utf-8");
+
+        raw_output
+            .lines()
+            .for_each(|line| stdout.push(line.to_string()));
+
+        stdout
+    }
 }
 
 impl<'a> SimpleCommand for SystemCommand<'a> {
     // Runs the command by constructing the qualified command
     // from the command name and arguments.
-    fn run(&self) -> io::Result<String> {
+    fn run(&self) -> io::Result<Vec<String>> {
         let mut command = process::Command::new(&self.name);
 
         if !self.arguments.is_empty() {
@@ -46,7 +57,7 @@ impl<'a> SimpleCommand for SystemCommand<'a> {
             .expect(&format!("Unable to execute {cmd}!", cmd = self.name));
 
         match out.status.code() {
-            Some(0) => Ok(String::from_utf8(out.stdout).expect("Failed to decode utf-8")),
+            Some(0) => Ok(self.parse_stdout(out.stdout)),
 
             // Everything that isn't a 0 return value is considered as a failure.
             _ => Err(Error::new(
--- pls-sys/src/directory.rs
+++ pls-sys/src/directory.rs
@@ -10,20 +10,11 @@ pub fn list_dir(dir: Option<&String>) -> io::Result<Vec<String>> {
         None => Some(get_current_dir()?),
     });
 
-    let out = command.run()?;
-    let files = out.lines();
-
-    // Must convert vector Lines to a vector of Strings
-    let mut files_str: Vec<String> = Vec::new();
-    files.for_each(|file| files_str.push(file.to_string()));
-
-    Ok(files_str)
+    Ok(command.run()?)
 }
 
 // Parses the absolute path of the current directory
 fn get_current_dir() -> io::Result<String> {
     let out = command::SystemCommand::new("pwd").run()?;
-    let current_dir = out.lines().next().expect("pwd failed");
-
-    Ok(String::from(current_dir))
+    Ok(String::from(out.first().expect("pwd returned nothing")))
 }
