4f6c922a2e505ee0d9decf61027d898f45522a27
Ahmed Sakr <ahmed@sakr.ca>
2020-09-13 11:57:45 -0400

    Prepare the structure of the configuration file

--- pls-classifier/src/configuration.rs
+++ pls-classifier/src/configuration.rs
@@ -1,15 +1,51 @@
 use std::env;
 use std::fs;
+use std::io::{self, Write};
 use std::path::Path;
 
-pub fn init() {
-    let home = format!("{}/.pls", env::var("HOME").unwrap());
-    let conf_dir = Path::new(&home);
+enum ConfigurationEntry {
+    Entry(String, String),
+}
 
-    if !conf_dir.exists() {
-        match fs::create_dir(conf_dir) {
-            Err(err) => panic!("Failed to initialize configuration: {}", err),
-            Ok(_) => (),
-        }
+pub fn init() {
+    let pls_conf = format!("{}/.pls/conf", env::var("HOME").unwrap());
+
+    // Check for pls conf dir
+    let path = Path::new(&pls_conf).parent().unwrap();
+    if !path.exists() {
+        fs::create_dir(path).expect("Failed to initialize conf dir");
     }
+
+    // Check for pls conf file
+    let path = Path::new(&pls_conf);
+    if !path.exists() {
+        fs::File::create(path).expect("Unable to create conf file");
+    }
+
+    write_default_configuration().expect("");
+}
+
+fn get_default_configuration() -> Vec<ConfigurationEntry> {
+    let mut config = Vec::new();
+    config.push(ConfigurationEntry::Entry("*.js".to_string(), "yellow".to_string()));
+    config.push(ConfigurationEntry::Entry("*.java".to_string(), "orange".to_string()));
+
+    config
+}
+
+fn write_default_configuration() -> io::Result<()>{
+    let pls_conf = format!("{}/.pls/conf", env::var("HOME").unwrap());
+    let mut file = fs::File::create(&pls_conf)?;
+
+    let config = get_default_configuration();
+    for entry in &config {
+        match entry {
+            ConfigurationEntry::Entry(key, value) => {
+                file.write(format!("{}={}", key, value).as_bytes()).expect("");
+            }
+        }
+
+        file.write("\n".as_bytes()).expect("");
+    }
+    Ok(())
 }
